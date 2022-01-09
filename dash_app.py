b6bddbca4c707521f443fe63dc6ed6b631d2f8b0
Adrien ANDRE <adrien.andre.ext@eiffage.com>
2022-01-09 20:25:38 +0100

    ajout du scraping et du graphique pieces/prix au m2

--- dash_app.py
+++ dash_app.py
@@ -2,17 +2,28 @@ import pandas as pd
 import plotly.express as px  # (version 4.7.0 or higher)
 import plotly.graph_objects as go
 from dash import Dash, html, dcc, Output, Input
+from paru_vendu_data import get_data_by_cp
 
-df = pd.read_csv("data/df.csv")
+# Données issues du dataset
+#df = pd.read_csv("data/df.csv")
+df = get_data_by_cp("75000")
+
+# Différents secteurs disponibles
+secteurs = df['code postal'].unique()
+
 
 app = Dash(__name__)
 server = app.server
 
 app.layout = html.Div([
 
-    html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
+    html.H1("Scraping des prix de l'immobilier", style={
+        'text-align': 'center',
+        'font-family': 'Roboto'
+    }),
 
-    dcc.Dropdown(id="slct_year",
+    # Prix des logements en fonction de la surface
+    dcc.Dropdown(id="surface_max",
                  options=[
                      {"label": "50 m2", "value": 50},
                      {"label": "100 m2", "value": 100},
@@ -26,20 +37,36 @@ app.layout = html.Div([
 
     html.Div(id='output_container', children=[]),
     html.Br(),
-
-    dcc.Graph(id='grah_surface_price', figure={}),
     
+    dcc.Graph(id='graph_surface_price', figure={}),
+    
+    
+    # Prix au m2 en fonction du nombre de pieces
+    html.Br(),
+    html.Div(id='output_container_2', children=[]),
+    
+    dcc.Dropdown(id="secteur",
+                options=[{"label": sec, "value": sec} for sec in secteurs],
+                multi=False,
+                value=secteurs[0],
+                style={'width': "40%"}
+                ),
+    dcc.Graph(id='graph_price_piece', figure={})
 
 ])
 
 # ------------------------------------------------------------------------------
 # Connect the Plotly graphs with Dash Components
 @app.callback(
-    [Output(component_id='output_container', component_property='children'),
-     Output(component_id='grah_surface_price', component_property='figure')],
-    [Input(component_id='slct_year', component_property='value')]
+    [
+        Output(component_id='output_container', component_property='children'),
+        Output(component_id='graph_surface_price', component_property='figure')
+    ],
+    [
+        Input(component_id='surface_max', component_property='value')
+    ]
 )
-def update_graph(option_slctd):
+def update_graph_surface_price(option_slctd):
     
     dff = df[df['surface'] < option_slctd ] 
     container = "The max surface chosen by user is: {}".format(option_slctd)
@@ -50,6 +77,29 @@ def update_graph(option_slctd):
 
 
     return container, fig
+
+
+@app.callback(
+    [
+        Output(component_id='output_container_2', component_property='children'),
+        Output(component_id='graph_price_piece', component_property='figure')
+    ],
+    [
+        Input(component_id='secteur', component_property='value')
+    ]
+)
+def update_graph_price_piece(option_slctd):
+    print(option_slctd)
+    df_price_surface = df[df['code postal'] == option_slctd].groupby("pieces").mean().reset_index()
+    print(df_price_surface)
+    
+    container = "Le secteur choisi est : {}".format(option_slctd)
+    
+    # Plotly Express
+    fig = px.bar(df_price_surface, x="pieces", y="prix au m2")
+
+    return container, fig
+
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
